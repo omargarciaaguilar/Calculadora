263c224b3133cd786866099d50afa30984d57a59
omargarciaaguilar <137461491+omargarciaaguilar@users.noreply.github.com>
2024-09-03 12:51:26 -0600

    Update CALCULADORA.C

--- CALCULADORA.C
+++ CALCULADORA.C
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int division(int ,int );
 
 
@@ -6,4 +8,48 @@ int division(int x, int y)
   int div;
   div = x / y;
   return div;
+}
+
+
+
+int main(){
+    
+    int opc
+    
+    do{
+        printf("que deseas hacer:\n");
+        
+        scanf("%d",&opc);
+        
+        switch(opc){
+            
+            case 1:
+            break;
+            
+            case 2:
+            break;
+            
+            case 3:
+            break;
+            
+            case 4:
+            break;
+            
+            case 5:
+            break;
+            
+            case 6:
+            printf("saliendo");
+            break;
+            
+            default:
+            printf("opcion no valida");
+            break;
+        }
+        
+    }
+    
+    
+    
+    return 0;
 }
