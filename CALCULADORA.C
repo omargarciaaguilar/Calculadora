9c03a9117d0d2e22b1d8caff040a641a4769fe3d
GabrielNavZap <141896739+GabrielNavZap@users.noreply.github.com>
2024-09-03 21:06:30 -0600

    Agregar su codigo dentro de la funcion que les tocó

--- CALCULADORA.C
+++ CALCULADORA.C
@@ -1,10 +1,36 @@
 #include <stdio.h>
 
-int division(int ,int );
-int suma(int , int );
-int resta(int , int );
-int multiplicacion(int ,int );
-int modulo(int ,int );
+float division(int x,int y);
+int suma(int x, int y);
+int resta(int x, int y);
+int multiplicacion(int x,int y);
+int modulo(int x,int y);
+
+float division(int x, int y)
+{
+
+}
+
+int suma(int x, int y)
+{
+
+}
+
+int resta(int x, int y)
+{
+
+}
+
+int multiplicacion(int x, int y)
+{
+
+}
+
+int modulo(int x, int y)
+{
+
+}
+
 
 int main()
 {
@@ -13,19 +39,53 @@ int main()
   {
       case 1:
             //SUMA
+      {
+        printf("Ingrese el primer sumando");
+        scanf("%d",&x);
+        printf("Ingrese el segundo sumando");
+        scanf("%d",&y);
+        printf("La suma: %d + %d = %d",x,y,suma(x,y));
+      }
       break;
       case 2:
             //RESTA
+      {
+        printf("Ingrese el primer entero");
+        scanf("%d",&x);
+        printf("Ingrese el segundo entero");
+        scanf("%d",&y);
+        printf("La resta: %d - %d = %d",x,y,resta(x,y));
+      }
       break;
       case 3:
             //MULTIPLICACION
+      {
+        printf("Ingrese el primer factor");
+        scanf("%d",&x);
+        printf("Ingrese el segundo factor");
+        scanf("%d",&y);
+        printf("La multiplicaci%cn: %d x %d = %d",162,x,y,multiplicacion(x,y));
+      }
       break;
       case 4:
             //DIVISION
+      {
+        printf("Ingrese el dividendo");
+        scanf("%d",&x);
+        printf("Ingrese el divisor");
+        scanf("%d",&y);
+        printf("La divisi%cn: %d / %d = %f",162,x,y,division(x,y));
+      }
       break;
       case 5:
             //MODULO
+      {
+        printf("Ingrese el primer entero");
+        scanf("%d",&x);
+        printf("Ingrese el segundo entero");
+        scanf("%d",&y);
+        printf("La operaci%cn: %d mod %d = %d",162,x,y,modulo(x,y));
+      }
       break;
   }
 }
-
